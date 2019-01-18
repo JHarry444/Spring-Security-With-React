40d26831b746a5823b5971a46ebf06c14514ef7a
jjharry444 <jordan.harrison@academytrainee.com>
2019-01-18 10:09:23 +0000

    Project created with Spring Init

---
+++ src/main/java/com/qa/SpringSecurityWithReact/SpringSecurityWithReactApplication.java
@@ -0,0 +1,14 @@
+package com.qa.SpringSecurityWithReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithReactApplication.class, args);
+	}
+
+}
+
